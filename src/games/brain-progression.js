802ea6423d3169a8f9e1866fc4f65a0ec13175b1
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-06-18 16:24:23 +0300

    Update "brain-progression" game

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -8,37 +8,45 @@ const limit = 10;
 
 const placeholder = '..';
 
+const genQuestion = () => {
+  const randomIndex = random(0, limit - 1);
+
+  const start = random(1, 50);
+  const end = start + limit * 2;
+
+  const sequence = range(start, end, step)
+    .map((number, idx) => {
+      if (idx === randomIndex) return placeholder;
+
+      return number;
+    })
+    .join(' ');
+
+  return sequence;
+};
+
+const genAnswer = (sequence) => {
+  const iter = (list, pointer) => {
+    if (pointer > list.length) return null;
+
+    const item = list[pointer];
+    const prevItem = list[pointer - 1];
+
+    if (item === placeholder) return toNumber(prevItem) + step;
+
+    return iter(list, pointer + 1);
+  };
+
+  return iter(sequence.split(' '), 0);
+};
+
 const game = ({
   description: 'What number is missing in the progression?',
-  genQuestion: () => {
-    const randomIndex = random(0, limit - 1);
+  genData: () => {
+    const question = genQuestion();
+    const answer = genAnswer(question);
 
-    const start = random(1, 50);
-    const end = start + limit * 2;
-
-    const sequence = range(start, end, step)
-      .map((number, idx) => {
-        if (idx === randomIndex) return placeholder;
-
-        return number;
-      })
-      .join(' ');
-
-    return sequence;
-  },
-  genAnswer: (sequence) => {
-    const iter = (list, pointer) => {
-      if (pointer > list.length) return null;
-
-      const item = list[pointer];
-      const prevItem = list[pointer - 1];
-
-      if (item === placeholder) return toNumber(prevItem) + step;
-
-      return iter(list, pointer + 1);
-    };
-
-    return iter(sequence.split(' '), 0);
+    return { question, answer };
   },
 });
 
