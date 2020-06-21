3a9adb61154072f23aeeaa18003b60b6318c5bd6
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-06-21 15:58:59 +0300

    Update "brain-even" game

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -2,15 +2,11 @@ import random from 'lodash/random.js';
 
 const isEven = number => number % 2 === 0;
 
-const genQuestion = () => random(1, 100);
-
-const genAnswer = number => (isEven(number) ? 'yes' : 'no');
-
 const game = ({
   description: 'Answer "yes" if the number is even, otherwise answer "no".',
   genData: () => {
-    const question = genQuestion();
-    const answer = genAnswer(question);
+    const question = random(1, 100);
+    const answer = isEven(question) ? 'yes' : 'no';
 
     return { question, answer };
   },
